c3ba242c17f47d7f09dbab9848a29a85d73b6465
Alexandre-Georges <alexandre.georges.ra@gmail.com>
2014-05-02 13:47:34 -0700

    Adding a client for HTTP calls

---
+++ utils/http-client.js
@@ -0,0 +1,41 @@
+var http = require('http');
+
+var HTTP_CODE_SUCCESS = 200;
+var SRC_REGEXP = /src="(.*)"/g;
+
+var HttpClient = function() {};
+HttpClient.get = function(path, callback) {
+    var request = http.get(path, function(response) {
+        if (response.statusCode !== HTTP_CODE_SUCCESS) {
+            console.log('bad status code (' + response.statusCode + ') during HTTP call for resource : ' + path);
+            callback(undefined, 'bad status code (' + response.statusCode + ')');
+        } else {
+            var responseContent = '';
+            response.setEncoding('UTF-8');
+            response.on('data', function(content) {
+                responseContent += content;
+            });
+            response.on('end', function() {
+                callback(responseContent, undefined);
+            });
+        }
+    });
+
+    request.on('error', function(error) {
+        console.log('error (' + error.message + ') during HTTP call for resource ' + path);
+        callback(undefined, error);
+    });
+
+    request.end();
+};
+
+HttpClient.extractSrcs = function(content) {
+    var srcs = [];
+    var result;
+    while(result = SRC_REGEXP.exec(content)) {
+        srcs.push(result[1]);
+    }
+    return srcs;
+};
+
+module.exports = HttpClient;
